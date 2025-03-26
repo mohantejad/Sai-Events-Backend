b5cac818e23cd29cf38b0ca7f02f61d66f25b590
Mohanteja Dharmavarapu <mohantejadharmavarapu@Mohantejas-MacBook-Pro.local>
2025-03-26 16:00:54 +0530

    Added allowed hosts

--- main/settings.py
+++ main/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = os.environ['DEBUG']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://event-manager-backend-production-c3c4.up.railway.app/']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
